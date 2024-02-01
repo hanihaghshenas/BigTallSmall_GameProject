543c54a1bf8f0d4b3482313a0bf65696faf76b4f
yasinghoreshi96 <58761915+yasinghoreshi96@users.noreply.github.com>
2024-02-02 03:06:47 +0330

    added abstracted player, movement

---
+++ Assets/Scripts/Models/Players/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    PhotonView View;
+    // bool crouch = false;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        View = GetComponent<PhotonView>();
+    }
+    void Update () {
+
+        if (View.IsMine)
+        {
+
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+            animator.SetFloat("Speed", Math.Abs(horizontalMove));
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+                animator.SetBool("isJumping", true);
+            }
+        }
+
+        // if (Input.GetButtonDown("Crouch"))
+        // {
+        //     crouch = true;
+        // } else if (Input.GetButtonUp("Crouch"))
+        // {
+        //     crouch = false;
+        // }
+
+    }
+
+    public void onLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+    void FixedUpdate ()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
