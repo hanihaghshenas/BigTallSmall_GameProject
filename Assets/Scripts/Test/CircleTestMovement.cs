150baee8856a49eb2bc7afe3b13a0db9ebd44b98
AmirReza <amirreza.25314@gmail.com>
2024-02-02 09:41:49 +0330

    adding Circle Test Movements

---
+++ Assets/Scripts/Test/CircleTestMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Map;
+using UnityEngine;
+
+namespace Test
+{
+    public class CircleTestMovement : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.gameObject.transform.position += new Vector3(0.2f, 0f, 0f);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.gameObject.transform.position += new Vector3(-0.2f, 0f, 0f);
+            }
+        }
+    }
+}
